e49cf3d71b6b2958b5d9130c3a73fce838f6a12c
Aditya Saraswat <saraswataditya664@gmail.com>
2025-10-28 17:36:48 +0530

    add traversal.c

---
+++ end term/array/traversal.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a[50], size;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+
+    for (int i=1;i<=size;i++){
+        printf("Enter the element %d of the array: ", i);
+        scanf("%d", &a[i]);
+    }
+
+    for (int i=1;i<=size;i++){
+        printf("Element %d: %d\n", i, a[i]);
+    }
+
+}
